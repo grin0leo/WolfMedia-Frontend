5ce01518569e56a0efbc94dd94b68c8eaf75ff7e
grin_leo <gridchin.leo@gmail.com>
2025-04-24 17:26:28 +0300

    fix: Исправить визуал на страницу Избранное

--- src/app/cases/fav/page.tsx
+++ src/app/cases/fav/page.tsx
@@ -11,19 +11,31 @@ export default function FavPage() {
 
     const favCases = useSelector((state: RootState) => state.fav.cases)
 
-    return (
+    return favCases.length > 0 ? (
         <main className={styles.container}>
-
             <h1 className={styles.label}>
                 Избранное: {favCases.length}
             </h1>
             <ul className={styles.column}>
                 {favCases.map((el: CaseType, index) => (
                     <li key={index}>
-                        <Case id={el.id} title={el.title} tags={el.tags} imgSrc={el?.poster?.image?.src} fullCase={el} />
+                        <Case
+                            id={el.id}
+                            title={el.title}
+                            tags={el.tags}
+                            imgSrc={el?.poster?.image?.src}
+                            fullCase={el}
+                        />
                     </li>
                 ))}
             </ul>
         </main>
+    ) : (
+        <main className={styles.container}>
+
+            <p className={styles.emptyMessage}>В избранном пока ничего нет</p>
+        </main>
+
     )
 }
+
